b73cbd998619e489bb0c00d3854171b6c470b5cd
Joel Zúñiga Jiménez <joelz2000@live.com>
2018-10-18 07:52:45 -0600

    Update Proyecto/Backend/DAL/ProductoDALInterface.cs
    Agregar interfaz producto

---
+++ Proyecto/Backend/DAL/ProductoDALInterface.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DAL
+{
+    public interface ProductoDALInterface
+    {
+        PRODUCTO mostrarProducto(int ID_PRODUCTO);
+        void agregarProducto(PRODUCTO producto);
+        void actualizarProducto(PRODUCTO producto);
+        void eliminarProducto(int ID_PRODUCTO);
+    }
+}
