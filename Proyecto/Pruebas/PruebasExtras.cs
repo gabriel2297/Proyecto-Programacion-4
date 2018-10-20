864f0e9a54aa2913dc24222e642b784979040412
Joel Zúñiga Jiménez <joelz2000@live.com>
2018-10-20 10:31:27 -0600

    Agregando text de extras
    Agregando metodo de prueba sobre agregar extra a la base de datos.

---
+++ Proyecto/Pruebas/PruebasExtras.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Backend.DAL;
+using Backend.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class PruebasExtras
+    {
+        ExtrasDALInterface extrasDAL;
+        EXTRA extra;
+        [TestMethod]
+        public void testAgregarExtra()
+        {
+            extrasDAL = new ExtrasDALImplementacion();
+            bool resultado = extrasDAL.agregarExtra(extra = new EXTRA()
+            {
+                NOMBRE_EXTRA = "Extra 1",
+                PRECIO_EXTRA = 500
+            });
+            Assert.AreEqual(true, resultado);
+        }
+    }
+}
