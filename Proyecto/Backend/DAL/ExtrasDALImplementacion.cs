785abad8f06eae48f619f7f02ef81bb784ef4bfd
Joel Zúñiga Jiménez <joelz2000@live.com>
2018-10-20 10:18:46 -0600

    Agrengando clase implementacion de extras
    Agregando metodos y funcionalidad al metodo agregarExtras

---
+++ Proyecto/Backend/DAL/ExtrasDALImplementacion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Model;
+
+namespace Backend.DAL
+{
+    public class ExtrasDALImplementacion : ExtrasDALInterface
+    {
+        PROIVEntities extraDB;
+
+        public bool actualizarExtra(EXTRA extra)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool agregarExtra(EXTRA extra)
+        {
+            using (extraDB = new PROIVEntities())
+            {
+                try
+                {
+                    extraDB.EXTRAS.Add(extra);
+                    extraDB.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+            }
+        }
+
+        public bool eliminarExtra(int id_extra)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EXTRA obtenerExtra(int id_extra)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
