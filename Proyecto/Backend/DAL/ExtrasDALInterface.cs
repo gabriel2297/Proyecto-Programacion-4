007267d2f8bd6b6a5b59e3c88b69c6df80e30d8c
Joel Zúñiga Jiménez <joelz2000@live.com>
2018-10-20 10:10:04 -0600

    Añadiendo interfaz sobre extras

---
+++ Proyecto/Backend/DAL/ExtrasDALInterface.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DAL
+{
+    public interface ExtrasDALInterface
+    {
+        EXTRA obtenerExtra(int id_extra);
+        bool agregarExtra(EXTRA extra);
+        bool actualizarExtra(EXTRA extra);
+        bool eliminarExtra(int id_extra);
+
+    }
+}
