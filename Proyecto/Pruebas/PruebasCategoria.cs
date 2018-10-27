c7420b4d92905bf70ac12598aa4d1172994fa0d6
Bryan <ulloab@gmail.com>
2018-10-27 16:15:56 -0600

    Agregando pruebas de Categorias y Mesas

---
+++ Proyecto/Pruebas/PruebasCategoria.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Backend.DAL;
+using Backend.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class PruebasCategoria
+    {
+        ICategoriaDAL categoriaDAL;
+        CATEGORIA categoria;
+        [TestMethod]
+        public void PruebaAgregar()
+        {
+            categoriaDAL = new CategoriaDALImpl();
+            bool resultado = categoriaDAL.addCategoria(categoria = new CATEGORIA()
+            {
+                ID_CATEGORIA = 2,
+                NOMBRE_CATEGORIA = "REFRESCOS",
+                DESCRIPCION = "NATURALES"
+            });
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+        public void pruebaActualizar()
+        {
+            categoriaDAL = new CategoriaDALImpl();
+            bool resultado = categoriaDAL.updateCategoria(categoria = new CATEGORIA()
+            {
+                ID_CATEGORIA = 2,
+                NOMBRE_CATEGORIA = "REFRESCOS",
+                DESCRIPCION = "GASEOSOS"
+            });
+
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+
+        public void pruebaEliminar()
+        {
+            categoriaDAL = new CategoriaDALImpl();
+            bool resultado = categoriaDAL.deleteCategoria(3);
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+
+        public void pruebaObtener()
+        {
+            categoriaDAL = new CategoriaDALImpl();
+            int idCategoria = categoriaDAL.obtenerCategoriaPorID(3).ID_CATEGORIA;
+
+            Assert.AreEqual(3, idCategoria);
+        }
+    }
+}
---
+++ Proyecto/Pruebas/PruebasMesa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Backend.DAL;
+using Backend.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class PruebasMesa
+    {
+        IMesaDAL mesaDAL;
+        MESA mesa;
+        [TestMethod]
+        public void PruebaAgregar()
+        {
+            mesaDAL = new MesaDALImpl();
+            bool resultado = mesaDAL.addMesa(mesa = new MESA()
+            {
+                ID_MESA = 2,
+                CAPACIDAD = 6,
+                ID_ESTADO = 2
+            });
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+        public void pruebaActualizar()
+        {
+            mesaDAL = new MesaDALImpl();
+            bool resultado = mesaDAL.updateMesa(mesa = new MESA()
+            {
+                ID_MESA = 2,
+                CAPACIDAD = 5,
+                ID_ESTADO = 1
+            });
+
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+
+        public void pruebaEliminar()
+        {
+            mesaDAL = new MesaDALImpl();
+            bool resultado = mesaDAL.deleteMesa(6);
+            Assert.AreEqual(true, resultado);
+        }
+
+        [TestMethod]
+
+        public void pruebaObtener()
+        {
+            mesaDAL = new MesaDALImpl();
+            int idMesa = mesaDAL.obtenerMesaPorId(2).ID_MESA;
+
+            Assert.AreEqual(2, idMesa);
+        }
+    }
+}
