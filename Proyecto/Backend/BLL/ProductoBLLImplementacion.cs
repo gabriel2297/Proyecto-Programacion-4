3165dd637fc4ec96b9c1f2118c23478b013ad27f
Joel Zúñiga Jiménez <joelz2000@live.com>
2018-10-29 08:48:26 -0600

    Agregar clases BLL de productos

---
+++ Proyecto/Backend/BLL/ProductoBLLImplementacion.cs
@@ -0,0 +1,72 @@
+﻿using Backend.DAL;
+using Backend.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.BLL
+{
+    public class ProductoBLLImplementacion : ProductoBLLInterface
+    {
+        ProductoDALInterface productoDAL;
+        public ProductoBLLImplementacion()
+        {
+            productoDAL = new ProductoDALImplementacion();
+        }
+        public bool actualizarProducto(PRODUCTO producto)
+        {
+           
+            try
+            {
+                productoDAL.actualizarProducto(producto);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
+
+        }
+
+        public bool agregarProducto(PRODUCTO producto)
+        {
+            try
+            {
+                productoDAL.agregarProducto(producto);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool eliminarProducto(int ID_PRODUCTO)
+        {
+            try
+            {
+                productoDAL.eliminarProducto(ID_PRODUCTO);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public PRODUCTO mostrarProducto(int idProducto)
+        {
+            try
+            {
+                return productoDAL.mostrarProducto(idProducto);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Proyecto/Backend/BLL/ProductoBLLInterface.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.BLL
+{
+    public interface ProductoBLLInterface
+    {
+        PRODUCTO mostrarProducto(int ID_PRODUCTO);
+        bool agregarProducto(PRODUCTO producto);
+        bool actualizarProducto(PRODUCTO producto);
+        bool eliminarProducto(int ID_PRODUCTO);
+    }
+}
