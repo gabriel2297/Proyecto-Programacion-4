2d5e2150f96af89360afc19b285cc17cf5524bec
Joel Zúñiga Jiménez <joelz2000@live.com>
2018-11-02 10:50:40 -0600

    Cambios en la interfaz BLL de extras

--- Proyecto/Backend/BLL/IExtrasBLL.cs
+++ Proyecto/Backend/BLL/IExtrasBLL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Backend.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace Backend.BLL
 {
     public interface IExtrasBLL
     {
+        bool agregarExtra(EXTRA extra);
+        bool actualizarExtra(EXTRA extra);
+        bool eliminarExtra(int id_extra);
+        List<EXTRA> GetEXTRAS();
+        EXTRA GetEXTRA(int id_extra);
 
     }
 }
