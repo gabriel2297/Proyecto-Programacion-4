a0d19ef2396397fea6567e07d2a510d058376395
Gabriel <gabrieluf2297@gmail.com>
2018-12-04 22:51:20 -0600

    Agregando trigger

--- SQL_Scripts/script_proyecto.sql
+++ SQL_Scripts/script_proyecto.sql
@@ -84,9 +84,18 @@ CREATE TABLE PRODUCTO_X_ORDEN (
 	ID_ORDEN INT NOT NULL,
 	CANTIDAD_PRODUCTO INT NOT NULL,
 	PRIMARY KEY (ID_PRODUCTO, ID_ORDEN),
-	FOREIGN KEY (ID_PRODUCTO) REFERENCES PRODUCTOS(ID_PRODUCTO),
 	FOREIGN KEY (ID_ORDEN) REFERENCES ORDENES(ID_ORDEN)
 );
+
+/* ver cual es el nombre del constraint del foreign key de ID_ORDEN para la tabla PRODUCTO_X_ORDEN con el siguiente comando */
+
+select CONSTRAINT_NAME
+from INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+where TABLE_NAME = 'PRODUCTO_X_ORDEN';
+
+/* eliminar ese constraint con el comando de abajo, el FK_ORDENES_ID_OR se saca del comando de arriba (el select) */
+-- ALTER TABLE ORDENES DROP CONSTRAINT FK__ORDENES__ID_OR__xxxxxxx;
+
 
 CREATE TABLE FACTURAS (
 	ID_FACTURA INT IDENTITY(1,1),
@@ -150,11 +159,6 @@ INSERT INTO ordenes (nombre_orden, id_mesero, id_mesa) VALUES ('Juan', 2, 3);
 INSERT INTO ordenes (nombre_orden, id_mesero, id_mesa) VALUES ('Luis', 3, 2);
 INSERT INTO ordenes (nombre_orden, id_mesero, id_mesa) VALUES ('Pedro', 4, 5);
 
-select * from ordenes;
-
--- 
-SELECT IDENT_CURRENT('table_name')+1; 
-
 -- productos en la orden
 INSERT INTO producto_x_orden (id_producto, id_orden, cantidad_producto) VALUES (4, 1, 3);
 INSERT INTO producto_x_orden (id_producto, id_orden, cantidad_producto) VALUES (3, 1, 1);
@@ -166,7 +170,12 @@ INSERT INTO producto_x_orden (id_producto, id_orden, cantidad_producto) VALUES (
 INSERT INTO producto_x_orden (id_producto, id_orden, cantidad_producto) VALUES (2, 2, 1);
 INSERT INTO producto_x_orden (id_producto, id_orden, cantidad_producto) VALUES (3, 2, 3);
 
-select * from PRODUCTO_X_ORDEN where id_orden = 1;
+
+INSERT INTO producto_x_orden (id_producto, id_orden, cantidad_producto) VALUES (2, (IDENT_CURRENT('ORDENES') + 1), 1);
+INSERT INTO producto_x_orden (id_producto, id_orden, cantidad_producto) VALUES (3, (IDENT_CURRENT('ORDENES') + 1), 3);
+
+INSERT INTO ordenes (nombre_orden, id_mesero, id_mesa) VALUES ('Prueba trigger', 4, 1);
+
 
 --FACTURACION
 INSERT INTO FACTURAS (PRECIO_UNIDAD,PRECIO_TOTAL,METODO_PAGO,MENSAJE,NOMBRE_CAJERO,TOTAL_CANCELADO,FECHA,ID_ORDEN) 
@@ -184,8 +193,28 @@ BEGIN
 	WHERE nombre_producto LIKE '%' + @string_busqueda + '%';
 END;
 
-DECLARE
-	@string_busqueda VARCHAR(Max)
-	SELECT @string_busqueda = 'Arr'
-EXEC buscarProductos
-	@string_busqueda
+CREATE OR ALTER TRIGGER crear_factura
+ON ordenes
+AFTER INSERT 
+AS
+BEGIN
+	-- declarar variables 
+	DECLARE @id_orden INTEGER;
+	DECLARE @nombre_mesero VARCHAR(50);
+	DECLARE @monto_total MONEY;
+	-- obtener el ultimo ID insertado en ordenes
+	SELECT @id_orden = IDENT_CURRENT('ORDENES');
+	-- sacar de la orden el nombre del mesero
+	SET @nombre_mesero = (SELECT a.nombre_mesero
+					      FROM MESEROS a, ORDENES o
+					      WHERE a.ID_MESERO = o.ID_MESERO
+					      AND o.ID_ORDEN = @id_orden);
+	-- calcular el monto total
+	SET @monto_total = (SELECT SUM((a.cantidad_producto * b.precio_producto))
+						FROM producto_x_orden a, productos b
+						WHERE a.ID_PRODUCTO = b.ID_PRODUCTO
+						AND a.ID_ORDEN = @id_orden);
+	-- crear la factura
+	INSERT INTO FACTURAS (PRECIO_UNIDAD,PRECIO_TOTAL,METODO_PAGO,MENSAJE,NOMBRE_CAJERO,TOTAL_CANCELADO,FECHA,ID_ORDEN) 
+	VALUES(@monto_total, @monto_total, 'Por defecto', 'Gracias por su compra', 'Sistema', @monto_total, GETDATE(), @id_orden);
+END;
