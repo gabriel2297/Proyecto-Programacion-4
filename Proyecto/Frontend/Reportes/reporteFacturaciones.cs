adc58702c3c01778062c1dc1a4509fb5423873ef
Joel Zúñiga Jiménez <joelz2000@live.com>
2018-12-07 17:29:05 -0600

    Update reporteFacturaciones.cs

--- Proyecto/Frontend/Reportes/reporteFacturaciones.cs
+++ Proyecto/Frontend/Reportes/reporteFacturaciones.cs
@@ -21,7 +21,6 @@ namespace Frontend.Reportes
         {
 
             this.reportViewer1.RefreshReport();
-            this.reportViewer2.RefreshReport();
         }
 
         private void btnRegresarFacturas_Click(object sender, EventArgs e)
