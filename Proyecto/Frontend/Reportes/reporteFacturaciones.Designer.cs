bde773638bbc5049589f7579263ac262d7fb745b
herrera10 <41028500+herrera10@users.noreply.github.com>
2018-12-10 14:00:34 -0600

    Reporte de Facturacion por fecha
    Correr el sp que esta al final del script .

---
+++ Proyecto/Frontend/Reportes/reporteFacturaciones.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Frontend.Reportes
+{
+    partial class reporteFacturaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btnRegresarFacturas = new System.Windows.Forms.Button();
+            this.FACTURABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cATEGORIABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.FACTURABindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cATEGORIABindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DatasetFacturas";
+            reportDataSource1.Value = this.FACTURABindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Frontend.Reportes.ReporteFacturacion.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(12, 12);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(673, 461);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // btnRegresarFacturas
+            // 
+            this.btnRegresarFacturas.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRegresarFacturas.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnRegresarFacturas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegresarFacturas.FlatAppearance.BorderSize = 0;
+            this.btnRegresarFacturas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegresarFacturas.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresarFacturas.ForeColor = System.Drawing.Color.White;
+            this.btnRegresarFacturas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegresarFacturas.Location = new System.Drawing.Point(12, 478);
+            this.btnRegresarFacturas.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegresarFacturas.Name = "btnRegresarFacturas";
+            this.btnRegresarFacturas.Size = new System.Drawing.Size(170, 46);
+            this.btnRegresarFacturas.TabIndex = 9;
+            this.btnRegresarFacturas.Text = "Regresar";
+            this.btnRegresarFacturas.UseVisualStyleBackColor = false;
+            this.btnRegresarFacturas.Click += new System.EventHandler(this.btnRegresarFacturas_Click);
+            // 
+            // FACTURABindingSource
+            // 
+            this.FACTURABindingSource.DataSource = typeof(Backend.Model.FACTURA);
+            // 
+            // cATEGORIABindingSource
+            // 
+            this.cATEGORIABindingSource.DataSource = typeof(Backend.Model.CATEGORIA);
+            // 
+            // reporteFacturaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(694, 535);
+            this.Controls.Add(this.btnRegresarFacturas);
+            this.Controls.Add(this.reportViewer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "reporteFacturaciones";
+            this.Text = "reporteFacturaciones";
+            this.Load += new System.EventHandler(this.reporteFacturaciones_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FACTURABindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cATEGORIABindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button btnRegresarFacturas;
+        private System.Windows.Forms.BindingSource FACTURABindingSource;
+        private System.Windows.Forms.BindingSource cATEGORIABindingSource;
+    }
+}
---
+++ Proyecto/Frontend/Reportes/reporteFacturaciones.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Frontend.Reportes
+{
+    public partial class reporteFacturaciones : Form
+    {
+        int id;
+        public reporteFacturaciones()
+        {
+            InitializeComponent();
+        }
+
+        public void obtenerIDFactura(int id_factura)
+        {
+            id = id_factura;
+           
+        }
+
+        private void reporteFacturaciones_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void btnRegresarFacturas_Click(object sender, EventArgs e)
+        {
+            this.Close();
+           
+        }
+    }
+}
